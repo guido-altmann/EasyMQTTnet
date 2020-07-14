645b228b0d420d354c3f2a308cc05d539ae5ff36
guido-altmann <guidoa@live.de>
2020-07-14 18:02:45 +0200

    Implement connection string parser

--- EasyMQTTnet/EasyMqttFactory.cs
+++ EasyMQTTnet/EasyMqttFactory.cs
@@ -1,7 +1,9 @@
-﻿namespace EasyMQTTnet
+﻿using System.Text.RegularExpressions;
+
+namespace EasyMQTTnet
 {
     /// <summary>
-    /// 
+    /// Factory to create the core APIs.
     /// </summary>
     public static class EasyMqttFactory
     {
@@ -12,7 +14,26 @@
         /// <returns></returns>
         public static IBus CreateBus(string connectionString)
         {
-            return new MessageBus();
+            var server = "localhost";
+            var port = 1883;
+
+            var pattern = @"(?<key>[^=;,]+)=(?<val>[^;,]+(,\d+)?)";
+            var options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(connectionString, pattern, options))
+            {
+                switch (m.Groups["key"].Value)
+                {
+                    case "server":
+                        server = m.Groups["val"].Value;
+                        break;
+                    case "port":
+                        _ = int.TryParse(m.Groups["val"].Value, out port);
+                        break;
+                }
+            }
+
+            return new MessageBus(server, port);
         }
     }
 }
